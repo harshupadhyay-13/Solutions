2f21408b0a1daee6c2b9974403ca52a4ab444f41
Harsh Vardhan Upadhyay <73693024+harshupadhyay-13@users.noreply.github.com>
2024-03-04 16:52:33 +0530

    Time: 138 ms (6.56%), Space: 46.1 MB (8.06%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String fixedString = "";
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)|| Character.isLetter(c)){
+                fixedString += c;
+            }
+        }        
+        fixedString = fixedString.toLowerCase();
+        int frontPointer =0, backPointer=fixedString.length()-1;
+        
+        while(frontPointer <= backPointer){
+            if(fixedString.charAt(frontPointer) != fixedString.charAt(backPointer)){
+                return false;
+            }
+            frontPointer++;
+            backPointer--;
+        }
+        return true;
+    }
+}
